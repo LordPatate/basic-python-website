4375b3df6f01ecc38b09c206038e1a773d3ac40d
LordPatate <tom.bricaud1@orange.fr>
2024-12-29 17:45:53 +0000

    Log received requests

--- src/server.py
+++ src/server.py
@@ -1,18 +1,22 @@
 from datetime import date
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
 from pathlib import Path
 
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        address = self.address_string()
+        logging.info("GET from %s", address)
         template = Path("html/index.html").read_text()
         return template.format(
             date=date.today(),
-            ip_address=self.address_string()
+            ip_address=address
         )
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, MyHandler)
     httpd.serve_forever()
